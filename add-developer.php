cef1e40de879364276f3eb67bf09f93b4814466f
OmarEl-Jundi <omar.eljundi@hotmail.com>
2023-08-01 21:54:23 +0300

    added ajax to add dev

--- add-developer.php
+++ add-developer.php
@@ -12,36 +12,35 @@ if ($_SESSION["user_role"] != 1) {
 }
 ?>
 <H1>Add a new Developer to the Database</H1>
-<form method="post" action="" enctype="multipart/form-data">
-    <p>Name: <input type="text" name="name"></p>
-    <p>Description: <input type="text" name="desc"></p>
-    <p><input type="submit" name="sb" value="insert"></p>
-    <p><input type="reset" value="clear all"></p>
-</form>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    require 'connection.php';
-    $name = $_POST['name'];
-    $desc = mysqli_real_escape_string($con, $_POST['desc']);
+<p>Name: <input type="text" id="name"></p>
+<p>Description: <input type="text" id="desc"></p>
+<p><button onclick="addDev()" name="sb">Insert</button></p>
+<p><input type="reset" value="clear all"></p>
 
-    if (empty($name)) {
-        echo "Please Enter The Name";
-    } else if (empty($desc)) {
-        echo "Please add a description";
-    } else {
-        $query = "INSERT INTO `Developer` (`name`, `description`) VALUES ('$name','$desc')";
-
-        if (mysqli_query($con, $query)) {
-            echo "The Developer $name is successfully added";
-        } else {
-            echo "Error executing query: " . mysqli_error($con);
-        }
-    }
-}
-
-?>
+<div id="ajaxDiv"></div>
 <p><a href="list-games.php">Show all Games</a></p>
 <p><a href="admin-home.php">Go Back to Main Menu</a></p>
+<script>
+    function addDev() {
+        var ajaxRequest = new XMLHttpRequest();
+        ajaxRequest.onreadystatechange = function() {
+            if (ajaxRequest.readyState == 4) {
+                var ajaxDisplay = document.getElementById('ajaxDiv');
+                ajaxDisplay.innerHTML = ajaxRequest.responseText;
+            }
+        }
+        var name = document.getElementById('name').value;
+        var desc = document.getElementById('desc').value;
+        if (name == "" || desc == "") {
+            alert("Please fill all fields");
+            return;
+        } else {
+            var queryString = "?name=" + name + "&desc=" + desc;
+            ajaxRequest.open("GET", "add-dev-proccess.php" + queryString, true);
+            ajaxRequest.send(null);
+        }
+    }
+</script>
+
+</html>
